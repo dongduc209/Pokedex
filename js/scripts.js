65716c42b4b0ed5da4577d5b56441d9eb3df0a86
dongduc209 <dongduc2091995@gmail.com>
2023-04-29 19:26:53 -0400

    Updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 let pokemonList = [
-  {name: "Kyogre", height: 4.5, types: ["water", "ice"]},
-  {name: "Groudon", height: 3.5, types: ["ground", "fire"]},
-  {name: "Rayquaza", height: 7, types: ["dragon", "flying"]}  
-  ]
+  { name: "Kyogre", height: 4.5, types: ["water", "ice"]},
+  { name: "Groudon", height: 3.5, types: ["ground", "fire"]},
+  { name: "Rayquaza", height: 7, types: ["dragon", "flying"]}  
+  ];
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 4.5) {
+    document.write(pokemonList[i].name + "(" + "height:" + " "+ pokemonList[i].height+ ")" + " - " + "Wow!!! This is a big Pokemon!" + "<br>" + "<br>") 
+  }else {
+    document.write(pokemonList[i].name + "(" + "height:" + " "+ pokemonList[i].height+ ")" + "<br>" + "<br>")
+  }
+}
