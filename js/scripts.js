7881d1e07b87e7c72741105bce51ce1459449b18
dongduc209 <dongduc2091995@gmail.com>
2023-04-28 20:40:26 -0400

    Update scripts.js
    Updated pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let fovoriteFood = 'Taco';
-document.write(fovoriteFood);
+let pokemonList = [
+  {name: "Kyogre", height: 4.5, types: ["water", "ice"]},
+  {name: "Groudon", height: 3.5, types: ["ground", "fire"]},
+  {name: "Rayquaza", height: 7, types: ["dragon", "flying"]}  
+  ]
