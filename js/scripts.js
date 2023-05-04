f456cdf3aa36a5d578c0f5f4b918aaede5748422
dongduc209 <dongduc2091995@gmail.com>
2023-05-03 21:54:18 -0400

    Replaced for to forEach()

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-let pokemonList = [
-  { name: "Kyogre", height: 4.5, types: ["water", "ice"]},
-  { name: "Groudon", height: 3.5, types: ["ground", "fire"]},
-  { name: "Rayquaza", height: 7, types: ["dragon", "flying"]}  
-  ];
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 4.5) {
-    document.write(pokemonList[i].name + "(" + "height:" + " "+ pokemonList[i].height+ ")" + " - " + "Wow!!! This is a big Pokemon!" + "<br>" + "<br>") 
-  }else {
-    document.write(pokemonList[i].name + "(" + "height:" + " "+ pokemonList[i].height+ ")" + "<br>" + "<br>")
-  }
-}
+ let pokemonList = [
+    { name: "Kyogre", height: 4.5, types: ["water", "ice"]},
+    { name: "Groudon", height: 3.5, types: ["ground", "fire"]},
+    { name: "Rayquaza", height: 7, types: ["dragon", "flying"]}  
+    ];
+
+  pokemonList.forEach(function(pokemon) {
+    document.write(pokemon);
+  });
+  
+
+
