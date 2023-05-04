7e87c9de3f22efee555e4ccd458ac37eadb7b493
dongduc209 <dongduc2091995@gmail.com>
2023-05-03 22:20:30 -0400

    Updated IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
- let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
     { name: "Kyogre", height: 4.5, types: ["water", "ice"]},
     { name: "Groudon", height: 3.5, types: ["ground", "fire"]},
     { name: "Rayquaza", height: 7, types: ["dragon", "flying"]}  
     ];
 
-  pokemonList.forEach(function(pokemon) {
-    document.write(pokemon);
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+      return pokemonList;
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon)
   });
-  
-
-
