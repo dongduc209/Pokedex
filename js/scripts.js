3204407e1fb6832d8acfe0b6a9d3691ccceabea3
dongduc209 <dongduc2091995@gmail.com>
2023-05-08 15:52:59 -0400

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,35 @@ let pokemonRepository = (function () {
       return pokemonList;
     }
 
+    function addListItem(pokemon){
+      let pokemonList = document.querySelector(".pokemon-list");
+      let listpokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("button-class");
+      listpokemon.appendChild(button);
+      pokemonList.appendChild(listpokemon);
+
+      button.addEventListener("click", function() {
+        showDetails(pokemon);});
+    }
+
+    function showDetails(pokemon){
+      document.write(pokemon);
+    }
+
+  
     return {
       add: add,
-      getAll: getAll
+      getAll: getAll,
+      addListItem: addListItem,
+      showDetails: showDetails,
     };
   })();
 
-  pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write(pokemon)
+  pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
   });
+
