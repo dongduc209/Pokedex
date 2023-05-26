c846215411b0ae020fc54aedd517f0f6f750ac39
dongduc209 <dongduc2091995@gmail.com>
2023-05-25 21:51:45 -0400

    updated nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ let pokemonRepository = (function () {
     function addListItem(pokemon) {
       let pokemonList = document.querySelector(".list-group");
       let listpokemon = document.createElement("li");
+      listpokemon.classList.add("list-item");
       let button = document.createElement("button");
       button.innerText = pokemon.name;
       button.classList.add("button-class", "btn", "btn-warning");
@@ -89,7 +90,35 @@ let pokemonRepository = (function () {
       modal.appendChild(myImage);
       modalBody.appendChild(modal);
       modalBody.appendChild(pokemonHeight);
-    }    
+    } 
+    
+    function searchPokemon() {
+      let searchInput = document.getElementById("search-input");
+      let searchText = searchInput.value.toLowerCase();
+      let allPokemon = document.querySelectorAll(".list-item");
+  
+      allPokemon.forEach(function (pokemon) {
+        let pokemonText = pokemon.querySelector(".button-class").innerText.toLowerCase();
+        let searchList = document.querySelector(".list-group");
+  
+        if (pokemonText.includes(searchText)) {
+          searchList.classList.add("search-list");
+          pokemon.style.display = "inline-block";
+        } else {
+          pokemon.style.display = "none";
+        }
+  
+        if (!searchInput.value) {
+          searchList.classList.remove("search-list");
+        }
+      });
+    }
+  
+    let searchInput = document.getElementById("search-input");
+    searchInput.addEventListener("input", function () {
+      searchPokemon();
+    });
+
     return {
       add: add,
       getAll: getAll,
